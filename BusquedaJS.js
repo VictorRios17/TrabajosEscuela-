a80822b01b6ac1f3742d1a867e330531e36a10bf
VictorRios17 <victor1ariel2rios3martinez45@gmail.com>
2025-09-04 23:10:19 -0700

    Add files via upload

---
+++ BusquedaJS.js
@@ -0,0 +1,9 @@
+let numeros = [1, 3, 5, 7, 9];
+let buscando = 1;
+
+if (numeros.includes(buscando)){
+    console.log("Numero encontrado")    
+    
+}else {
+    console.log("Numero no encontrado");
+}
