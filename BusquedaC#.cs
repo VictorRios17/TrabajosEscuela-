c6e2fdf94efff4155802bab78d77717651b62f1a
VictorRios17 <victor1ariel2rios3martinez45@gmail.com>
2025-09-04 23:32:03 -0700

    Add files via upload

---
+++ BusquedaC#.cs
@@ -0,0 +1,20 @@
+using System;
+class Busqueda {
+  static void Main() {
+    int [] numeros = {1, 3, 5, 7, 9};
+    int buscando = 7;
+    bool encontrado = false;
+    
+    foreach (int num in numero) {
+        if (num == buscando){
+            encontrado == true;ç
+            break;
+        }
+    }
+    
+    if (encontrado)
+        console.WriteLine("Numero encontrado");
+    else 
+     console.WriteLine("Numero  no encontrado");
+  }
+}
